436340bbfeee940662d6379224c32b95b247c8ac
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2023-01-14 15:42:21 +0100

    update

--- app/Models/Promotional_ads.php
+++ app/Models/Promotional_ads.php
@@ -9,6 +9,17 @@ class Promotional_ads extends Model
 {
     use HasFactory;
 
-    protected $table = "atlas_promotional_ads";
-    protected $fillable = ['category_id', 'name', 'pdf_url', 'description', 'image_url','status', 'created_at', 'updated_at', 'deleted_at'];
+    protected $table = 'atlas_promotional_ads';
+    protected $fillable = [
+        'category_id',
+        'name',
+        'pdf_url',
+        'type',
+        'description',
+        'image_url',
+        'status',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
