f5bf249e363faead7bc3e0294962f74ef5f6ef04
Kalu Chinonso <tripletens.kc@gmail.com>
2023-03-13 12:31:08 +0100

    add the account id for email

--- app/Http/Controllers/DealerController.php
+++ app/Http/Controllers/DealerController.php
@@ -1109,13 +1109,13 @@ class DealerController extends Controller
     {
         //valid credential
         $this->validate($request, [
-            'email' => 'required|email',
+            'account_id' => 'required',
             'password' => 'required|min:6',
         ]);
 
         if (
             !($token = Auth::guard('api')->attempt([
-                'email' => $request->email,
+                'account_id' => $request->account_id,
                 'password' => $request->password,
             ]))
         ) {
@@ -1125,7 +1125,7 @@ class DealerController extends Controller
         }
 
         $active_staff = Dealer::query()
-            ->where('email', $request->email)
+            ->where('account_id', $request->account_id)
             ->get()
             ->first();
 
@@ -1135,10 +1135,10 @@ class DealerController extends Controller
             return response()->json($this->result);
         }
 
-        $dealer = Dealer::where('email', $request->email)->first();
+        $dealer = Dealer::where('account_id', $request->account_id)->first();
         $dealer->role = 'dealer';
 
-        $dealer_details = Dealer::where('email', $request->email)->get();
+        $dealer_details = Dealer::where('account_id', $request->account_id)->get();
 
         $dealer_details[0]->update([
             'last_login' => Carbon::now(),
