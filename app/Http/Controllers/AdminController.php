95ba290d221c98960a0818fbf1a1c60f90fdbab9
Kalu Chinonso <tripletens.kc@gmail.com>
2023-05-06 02:07:00 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4286,11 +4286,11 @@ class AdminController extends Controller
             foreach ($all_carded_not_submitted_products as $carded_data) {
                 $data = json_decode($carded_data->data);
                 foreach ($data as $value) {
-                    $total_not_submitted_in_cart =
-                        $total_not_submitted_in_cart + 1;
-
-                    $total = $value->total;
-                    $total_not_submitted_in_cart_amt += $total;
+                    $total_not_submitted_in_cart = $total_not_submitted_in_cart + 1;
+                    if($value->total){
+                         $total = $value->total;
+                         $total_not_submitted_in_cart_amt += $total;
+                    }
                 }
             }
         }
